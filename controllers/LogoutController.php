582d37a6d5643f8976706249f7391bb53b3b554b
Ntabethemba Ntshoza <nntshoza@mrppricegroup.com>
2024-03-11 13:41:31 +0200

    Fixing the conflicts

--- controllers/LogoutController.php
+++ controllers/LogoutController.php
@@ -21,25 +21,11 @@ class LogoutController
     public function logout()
     {
         $this->logger->pushHandler(new StreamHandler('var/System.log', Logger::DEBUG));
-<<<<<<< HEAD
 
         Session::destroy();
 
         $this->logger->info("User logged out at " . date('Y-m-d'));
 
-=======
-    
-        $this->flush();
-    
-        session_destroy();
-    
-        $params = session_get_cookie_params();
-        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-    
-        
-        $this->logger->info("User logged out at " . date('Y-m-d') );
-        
->>>>>>> d63a2764be67c9a28dd6c0d98b681cc0903d1571
         redirect('/');
         exit(); // Ensure that script execution stops after the redirect
     }
